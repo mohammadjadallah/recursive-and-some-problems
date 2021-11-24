241e65ee7880ff281a433b8bca1f1e86e64424c9
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-11-25 00:17:28 +0200

    area of the square
    Let us solve this problem together

--- area of the square problem.py
+++ area of the square problem.py
@@ -1,7 +1,7 @@
-# Problem! you teach children about how to calculate area and perimeter of the square
-# and you will solve 20 question about the way to training them.
-# but that will consume the time, so you want to write program to reduse from the time
-# whan calculate all 20 quest.
+# Problem! you teach children about how to calculate the area and perimeter of the square
+# and you will solve 20 questions about the way to find area and perimeter. to teach them.
+# but that will consume the time, so you want to write a program to reduce the time
+# when calculating all 20 quests.
 # Now try to solve it.
 # Hint: perimeter = (x * 4), area = (x ** 2) 
 
