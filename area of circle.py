0187a1a7845c976d15417673d3a76f000f52c846
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-11-27 11:17:32 +0200

    area of circle
    calculate the area of a circle

---
+++ area of circle.py
@@ -0,0 +1,8 @@
+# area of circle = pi(3.14159265359) * radius**2(π نق * نق)
+
+from math import pi
+print(f"area of circle is {pi * float(input('Enter radius: '))**2}")
+
+# in the above way we solved the problem with just one line after import library
+# you can solve it in many way.
+# good luck
