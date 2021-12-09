e2423be846d39d442bccb4070c1b323386eb09b3
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-12-10 00:43:14 +0200

    problem solving program make the value absolutely
    How to solve this problem:
    
    # Problem. Write a program to print the absolute value of a number entered by the user.
    # INPUT: 1        OUTPUT: 1
    # INPUT: -1        OUTPUT: 1

---
+++ absolute value.py
@@ -0,0 +1,10 @@
+# Problem. Write a program to print absolute value of a number entered by user.
+# INPUT: 1        OUTPUT: 1
+# INPUT: -1        OUTPUT: 1
+
+num = float(input('Enter number: '))
+
+if num > 0:
+    print(num)
+else:
+    print(num * -1)
